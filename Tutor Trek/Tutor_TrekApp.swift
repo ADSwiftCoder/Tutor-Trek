3c61283a7fc80704f48042c1835b4174e07ec1ab
Adrian Roger Menezes <adrianrogermenezes10@gmail.com>
2023-09-09 15:06:00 +0400

    Initial Commit

---
+++ Tutor Trek/Tutor_TrekApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tutor_TrekApp.swift
+//  Tutor Trek
+//
+//  Created by Adrian Menezes on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tutor_TrekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
